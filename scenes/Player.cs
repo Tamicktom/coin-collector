923835a7119a7109856a1359dffd79384c658d4b
Tamicktom <rickpokemon38@gmail.com>
2026-03-29 20:16:20 -0300

    add input mappings for player controls and update project features

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -5,16 +5,9 @@ public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
