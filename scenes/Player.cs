48ac26f7eccacb75c3685d380a2c5a4a841de4c4
Tamicktom <rickpokemon38@gmail.com>
2026-03-30 09:01:06 -0300

    refactor player movement logic to simplify velocity handling and ensure consistent sliding behavior

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -7,27 +7,8 @@ public partial class Player : CharacterBody2D
 	public const float JumpVelocity = -400.0f;
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		this.Velocity = new Vector2(10, 10);
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
+		this.MoveAndSlide();
 	}
 }
---
+++ scripts/Keys.cs
@@ -0,0 +1,13 @@
+
+
+/// <summary>
+/// Global keys used in the game
+/// </summary>
+
+public struct Keys
+{
+    public const string Left = "left";
+    public const string Right = "right";
+    public const string Up = "up";
+    public const string Down = "down";
+}
